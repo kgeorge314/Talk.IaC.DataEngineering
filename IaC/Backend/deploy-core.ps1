d70bcd593d9bb3188e7092a38befa7224da85dad
Kevin George <kgeorge.314@hotmail.com>
2023-05-31 00:17:06 +0100

    Adding outputs for progress tracking

--- IaC/Backend/deploy-core.ps1
+++ IaC/Backend/deploy-core.ps1
@@ -8,6 +8,7 @@ $location = "northeurope"
 
 
 # Setup AAD Application
+Write-Output "1. Creating AAD Application"
 $existingAdApp = (az ad app list --display-name $displayName  | ConvertFrom-Json | Where-Object -Property displayName -eq $displayName | Select-Object -First 1)
 if (-not ($existingAdApp)) {
     $existingAdApp = (az ad app create --display-name $displayName --sign-in-audience 'AzureADMyOrg' --enable-id-token-issuance true | ConvertFrom-Json)
@@ -15,17 +16,21 @@ if (-not ($existingAdApp)) {
 
 
 # Setup AAD Application SPN
+Write-Output "2. Creating Service Principal for $($existingAdApp.displayName)"
 $existingAdAppSpn = (az ad sp list --display-name $displayName | ConvertFrom-Json | Where-Object -Property appId -EQ $existingAdApp.appId)
 if(-not($existingAdAppSpn)){
-    $existingAdAppSpn = az ad sp create --id $existingAdApp.id
+    $null = az ad sp create --id $existingAdApp.id
+    $existingAdAppSpn = (az ad sp list --display-name $displayName | ConvertFrom-Json | Where-Object -Property appId -EQ $existingAdApp.appId)
 }
 
 # Setup Credential
+Write-Output "3. Creating Credential for Service Principal for $($existingAdAppSpn.displayName)"
 $secretExpiresIn2Hours = (Get-Date).ToUniversalTime().AddHours(2)
 $secretExpiresIn2HoursUnix = [int64](Get-Date $secretExpiresIn2Hours  -UFormat %s)   
 $azADSecretsPlain = az ad app credential reset --id $existingAdApp.id --display-name 'rbac' --end-date $secretExpiresIn2Hours.ToString("yyyy-MM-ddTHH:mm:ss+00:00") | ConvertFrom-Json
 
 # Setup Backend Core Resources 
+Write-Output "4. Deploying Core Resources to $resourceGroupName"
 $templateFile = "$PSScriptRoot/core-resources.bicep"
 $tenantId = (az account show | ConvertFrom-Json).tenantId
 
@@ -45,7 +50,6 @@ $parameters = @{
     "SPNSecretExpiry" = @{
         "value" = $secretExpiresIn2HoursUnix
     };
-    
 }
 
 az group create --name $resourceGroupName --location $location 
