fd0c9fdba450bcac6ff2377f8020d8d8f10fe09b
Kevin George <kgeorge.314@hotmail.com>
2023-06-06 23:29:55 +0100

    Adding outputs

--- IaC/Terraform/tf_outputs.tf
+++ IaC/Terraform/tf_outputs.tf
@@ -2,3 +2,13 @@ output "resource_group" {
   description = "The resource group where Azure resources for this environment are deployed"
   value       = azurerm_resource_group.rg
 }
+
+output "adf_main" {
+  description = "Main Azure Data Factory"
+  value       = azurerm_data_factory.adf_main
+}
+
+output "st_main" {
+  description = "Main Storage Account"
+  value       = azurerm_storage_account.st_main
+}
